69f570dbe6ba800fddb77fc9d526b3066016e067
James Miller <james@fractal-labs.com>
2019-02-27 10:24:27 +0000

    james - adding basic structure and router config

---
+++ src/components/Landing/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import '../../stylesheets/components/_landing.scss';
+
+const LandingPage = () => (
+    <div>
+        <h1>Welcome, this is the landing page</h1>
+    </div>
+);
+
+export default LandingPage;
